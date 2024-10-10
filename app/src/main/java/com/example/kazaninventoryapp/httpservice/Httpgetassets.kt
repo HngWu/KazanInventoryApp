39fa2ff54dfa16f717e7e9ce48ea94d1c15c2e21
HngWu <hngwutan@gmail.com>
2024-10-10 22:29:14 +0800

    Update Httpgetassets.kt

--- app/src/main/java/com/example/kazaninventoryapp/httpservice/Httpgetassets.kt
+++ app/src/main/java/com/example/kazaninventoryapp/httpservice/Httpgetassets.kt
@@ -7,6 +7,14 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
+//TODO 1: Implement the clear button for the search bar correctly by clearing the search query
+//TODO 2: Implement the landscape mode for the app where change in orientation does not affect the filters
+//TODO 3: Implement the generation of the new asset serial number when transferring an asset correctly
+//TODO 4: Implement the snackbar for the app
+//TODO 5: Implement display of multiple images correctly
+// git pull from laptop first
+
+
 class httpgetassets {
     fun getAssets(): MutableList<Asset>? {
         val url = URL("http://10.0.2.2:5232/api/Asset")
